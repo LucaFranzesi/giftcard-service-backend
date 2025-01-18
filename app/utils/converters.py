a8169e6044e0d18b68d36c72c3ff15fb8fd1ed48
Luca Franzesi <luca.franzesi@beatdata.it>
2025-01-18 22:15:36 +0100

    Update SqlAlchemy Serialization utils

--- app/utils/converters.py
+++ app/utils/converters.py
@@ -1,7 +1,16 @@
+from datetime import datetime
+
 def serialize_sqlalchemy_obj(obj):
     """
-    Converts SQLAlchemy object to a dictionary
+    Converts SQLAlchemy object to a dictionary, handling special types like datetime.
     """
     if hasattr(obj, '__table__'):
-        return {column.name: getattr(obj, column.name) for column in obj.__table__.columns}
-    raise ValueError("Obj non è un'istanza di un modello SQLAlchemy.")
+        result = {}
+        for column in obj.__table__.columns:
+            value = getattr(obj, column.name)
+            # Converte datetime in stringa ISO 8601
+            if isinstance(value, datetime):
+                value = value.isoformat()
+            result[column.name] = value
+        return result
+    raise ValueError("Object is not an instance of a SQLAlchemy object.")
