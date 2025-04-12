46265fd6f5de1580683de7d32c346f986bb78915
Luca Franzesi <luca.franzesi@beatdata.it>
2025-04-12 09:59:00 +0200

    Add basic CORS allowed origins for local development

--- app/main.py
+++ app/main.py
@@ -2,8 +2,24 @@ from routers import auth, gift_card
 from db import engine
 from fastapi import FastAPI
 from models.database import Base
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(gift_card.router)
